784446a347ca7c366e61f3800c0348936d162f11
DEVIISWARYAR <84283301+DEVIISWARYAR@users.noreply.github.com>
2021-05-16 21:59:59 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatDividerModule} from '@angular/material/divider';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    MatProgressBarModule,
+    MatDividerModule,
+    MatIconModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
